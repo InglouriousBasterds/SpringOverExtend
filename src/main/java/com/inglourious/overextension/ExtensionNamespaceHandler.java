2bc256190e1e859e5acb3cbba0249f658f44d43f
Carlo Corti <araknoid@gmail.com>
2018-11-18 12:10:12 +0100

    Refactoring

--- src/main/java/com/inglourious/overextension/ExtensionNamespaceHandler.java
+++ src/main/java/com/inglourious/overextension/ExtensionNamespaceHandler.java
@@ -3,12 +3,14 @@ package com.inglourious.overextension;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
 /**
- * @author dg gb The Class ExtensionNamespaceHandler configured in spring.handlers reads and registers the decorator @ExtensionBeanDefinitionDecorator
+ * The class {@code ExtensionNamespaceHandler} configured in spring.handlers reads and registers the {@link ExtensionBeanDefinitionDecorator} decorator
+ *
+ * @author dg gb
  */
 public class ExtensionNamespaceHandler extends NamespaceHandlerSupport {
 
     @Override
-	public void init() {
-		registerBeanDefinitionDecoratorForAttribute("extension", new ExtensionBeanDefinitionDecorator());
-	}
+    public void init() {
+        registerBeanDefinitionDecoratorForAttribute("extension", new ExtensionBeanDefinitionDecorator());
+    }
 }
