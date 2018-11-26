98326677ad429dba698db45955c7ae581ddcb8a4
Carlo Corti <araknoid@gmail.com>
2018-11-26 23:10:09 +0100

    Renaming

--- src/main/java/com/inglourious/overextension/annotation/ExtensionBeanDefinitionRegistryPostProcessor.java
+++ src/main/java/com/inglourious/overextension/annotation/ExtensionBeanDefinitionRegistryPostProcessor.java
@@ -49,10 +49,10 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
                     AnnotatedBean annotatedBean = new AnnotatedBean(beanDefinitionName, (ScannedGenericBeanDefinition) configurableListableBeanFactory.getBeanDefinition(beanDefinitionName));
 
                     String superClassName = annotatedBean.getSuperClassName()
-                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.getName() + " annotated with OverExtension must extend a superclass"));
+                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.name() + " annotated with OverExtension must extend a superclass"));
 
-                    List<String> parentBeanNames = parentBeanNamesRetriever.from(annotatedBean.getMetadata())
-                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.getName() + " must extends a spring bean component or specify extendBeanId , doesn't exist a spring bean for the class " + superClassName + " "));
+                    List<String> parentBeanNames = parentBeanNamesRetriever.from(annotatedBean.metadata())
+                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.name() + " must extends a spring bean component or specify extendBeanId , doesn't exist a spring bean for the class " + superClassName + " "));
 
                     Optional<ParentBean> parentBean = parentBeanNames.stream()
                             .map(name -> new ParentBean(name, configurableListableBeanFactory.getBeanDefinition(name)))
@@ -60,8 +60,8 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
                             .filter(pb -> pb.isSubClassOf(superClassName))
                             .findFirst();
 
-                    ReplacerKeyRegistry replacerKeyRegistry = parentBean.map(pb -> new ReplacerKeyRegistry(annotatedBean.getName(), annotatedBean.getDefinition(), pb.getName(), pb.getDefinition()))
-                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.getName() + " must extends a unique spring bean component  or specify extendBeanId. Invalid superClass " + superClassName + " (" + parentBeanNames.toString() + ")"));
+                    ReplacerKeyRegistry replacerKeyRegistry = parentBean.map(pb -> new ReplacerKeyRegistry(annotatedBean.name(), annotatedBean.definition(), pb.name(), pb.definition()))
+                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.name() + " must extends a unique spring bean component  or specify extendBeanId. Invalid superClass " + superClassName + " (" + parentBeanNames.toString() + ")"));
 
                     addInMapRegistry.add(replacerKeyRegistry);
                 }
@@ -80,6 +80,7 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
     }
 
     private class AnnotatedBean {
+
         private final String name;
         private final AnnotatedBeanDefinition definition;
 
@@ -88,7 +89,7 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
             this.definition = definition;
         }
 
-        public Optional<String> getSuperClassName() {
+        Optional<String> getSuperClassName() {
             String superClassName = definition.getMetadata().getSuperClassName();
             return isEmpty(superClassName) || isObjectClass(superClassName) ? empty() :
                     of(superClassName);
@@ -99,15 +100,15 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
         }
 
 
-        public AnnotationMetadata getMetadata() {
+        AnnotationMetadata metadata() {
             return definition.getMetadata();
         }
 
-        public AnnotatedBeanDefinition getDefinition() {
+        AnnotatedBeanDefinition definition() {
             return definition;
         }
 
-        public String getName() {
+        String name() {
             return name;
         }
     }
@@ -121,11 +122,11 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
             this.definition = definition;
         }
 
-        String getName() {
+        String name() {
             return name;
         }
 
-        BeanDefinition getDefinition() {
+        BeanDefinition definition() {
             return definition;
         }
 
