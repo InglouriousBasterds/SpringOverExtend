c50b264be4c90afaf369dfa32f1ef32e8e36cee7
Carlo Corti <araknoid@gmail.com>
2018-11-26 23:06:21 +0100

    Refactoring

--- src/main/java/com/inglourious/overextension/annotation/ExtensionBeanDefinitionRegistryPostProcessor.java
+++ src/main/java/com/inglourious/overextension/annotation/ExtensionBeanDefinitionRegistryPostProcessor.java
@@ -4,16 +4,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.ScannedGenericBeanDefinition;
+import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static java.lang.Class.forName;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 import static org.springframework.util.StringUtils.isEmpty;
 
 /**
@@ -42,16 +46,13 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
 
                 if (isBeanAnnotatedWithOverExtension(beanDefinitionName, configurableListableBeanFactory)) {
 
-                    ScannedGenericBeanDefinition beanDefinition = (ScannedGenericBeanDefinition) configurableListableBeanFactory.getBeanDefinition(beanDefinitionName);
+                    AnnotatedBean annotatedBean = new AnnotatedBean(beanDefinitionName, (ScannedGenericBeanDefinition) configurableListableBeanFactory.getBeanDefinition(beanDefinitionName));
 
-                    String superClassName = beanDefinition.getMetadata().getSuperClassName();
+                    String superClassName = annotatedBean.getSuperClassName()
+                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.getName() + " annotated with OverExtension must extend a superclass"));
 
-                    if (isEmpty(superClassName) || isObjectClass(superClassName)) {
-                        throw new BeanCreationException("Bean " + beanDefinitionName + " annotated with OverExtension must extend a superclass");
-                    }
-
-                    List<String> parentBeanNames = parentBeanNamesRetriever.from(beanDefinition.getMetadata())
-                            .orElseThrow(() -> new BeanCreationException("Bean " + beanDefinitionName + " must extends a spring bean component or specify extendBeanId , doesn't exist a spring bean for the class " + superClassName + " "));
+                    List<String> parentBeanNames = parentBeanNamesRetriever.from(annotatedBean.getMetadata())
+                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.getName() + " must extends a spring bean component or specify extendBeanId , doesn't exist a spring bean for the class " + superClassName + " "));
 
                     Optional<ParentBean> parentBean = parentBeanNames.stream()
                             .map(name -> new ParentBean(name, configurableListableBeanFactory.getBeanDefinition(name)))
@@ -59,8 +60,8 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
                             .filter(pb -> pb.isSubClassOf(superClassName))
                             .findFirst();
 
-                    ReplacerKeyRegistry replacerKeyRegistry = parentBean.map(pb -> new ReplacerKeyRegistry(beanDefinitionName, beanDefinition, pb.getName(), pb.getDefinition()))
-                            .orElseThrow(() -> new BeanCreationException("Bean " + beanDefinitionName + " must extends a unique spring bean component  or specify extendBeanId. Invalid superClass " + superClassName + " (" + parentBeanNames.toString() + ")"));
+                    ReplacerKeyRegistry replacerKeyRegistry = parentBean.map(pb -> new ReplacerKeyRegistry(annotatedBean.getName(), annotatedBean.getDefinition(), pb.getName(), pb.getDefinition()))
+                            .orElseThrow(() -> new BeanCreationException("Bean " + annotatedBean.getName() + " must extends a unique spring bean component  or specify extendBeanId. Invalid superClass " + superClassName + " (" + parentBeanNames.toString() + ")"));
 
                     addInMapRegistry.add(replacerKeyRegistry);
                 }
@@ -74,13 +75,41 @@ public class ExtensionBeanDefinitionRegistryPostProcessor implements BeanFactory
         }
     }
 
-    private boolean isObjectClass(String superClassName) {
-        return Object.class.getCanonicalName().equalsIgnoreCase(superClassName);
-    }
-
-
     private boolean isBeanAnnotatedWithOverExtension(String beanDefinitionName, ConfigurableListableBeanFactory configurableListableBeanFactory) {
         return configurableListableBeanFactory.findAnnotationOnBean(beanDefinitionName, OverExtension.class) != null;
+    }
+
+    private class AnnotatedBean {
+        private final String name;
+        private final AnnotatedBeanDefinition definition;
+
+        private AnnotatedBean(String name, AnnotatedBeanDefinition definition) {
+            this.name = name;
+            this.definition = definition;
+        }
+
+        public Optional<String> getSuperClassName() {
+            String superClassName = definition.getMetadata().getSuperClassName();
+            return isEmpty(superClassName) || isObjectClass(superClassName) ? empty() :
+                    of(superClassName);
+        }
+
+        private boolean isObjectClass(String superClassName) {
+            return Object.class.getCanonicalName().equalsIgnoreCase(superClassName);
+        }
+
+
+        public AnnotationMetadata getMetadata() {
+            return definition.getMetadata();
+        }
+
+        public AnnotatedBeanDefinition getDefinition() {
+            return definition;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
     private class ParentBean {
